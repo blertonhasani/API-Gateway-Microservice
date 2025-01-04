7797cd66d37b96f8833e9f80e09f67b5e379f0b5
blerton <blerton.hasani@clarivate.com>
2025-01-04 18:56:52 -0500

    Initial commit

---
+++ src/main/java/com/microcoreservices/microcoreservicesspringboot/MicroCoreServicesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.microcoreservices.microcoreservicesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCoreServicesSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroCoreServicesSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/microcoreservices/microcoreservicesspringboot/MicroCoreServicesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcoreservices.microcoreservicesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCoreServicesSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
